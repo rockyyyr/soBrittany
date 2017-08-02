be8448ee28fb069938b6079d693d7288eb2a265b
Rocky Robson <RockyRobson@hotmail.com>
2017-08-02 16:29:01 -0700

    spinning header toggle

--- script.js
+++ script.js
@@ -1,11 +1,25 @@
 $(document).ready(function(){
 
     $('.header-toggle').on('click', function(){
+        spinHeaderToggle();
         $('.drop-down-buttons').slideToggle('slow');
     });
 
     $('.drop-down-button').on('click', function(){
+        spinHeaderToggle();
         $('.drop-down-buttons').slideToggle('slow');
     });
 
 });
+
+function spinHeaderToggle(){
+    var element = $('.header-toggle');
+
+    if(element.hasClass('active-drop-down')){
+        element.removeClass('active-drop-down');
+        element.addClass('deactive-drop-down')
+    } else {
+        element.addClass('active-drop-down');
+        element.removeClass('deactive-drop-down');
+    }
+}
